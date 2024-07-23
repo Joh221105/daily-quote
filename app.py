0048fc138f7fa549fd3767d388c53e36e662cc97
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-07-23 15:29:39 -0400

    adds function to fetch daily quote from api and display it to html

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+def get_daily_quote():
+    url = 'https://zenquotes.io/api/today'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        quote_data = data[0]
+        quote = quote_data.get('q', 'No quote available.')
+        author = quote_data.get('a', 'Unknown author')
+        return f'"{quote}" - {author}'
+    else:
+        return 'Failed to retrieve quote'
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    quote = get_daily_quote()
+    return render_template('index.html', quote=quote)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
